7848e7b78dbf273d2404793ff4ba12910d8c3bb9
SMILEGATE\dooykang <dooykang@smilegate.com>
2025-07-09 18:04:18 +0900

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+//import styled from "styled-components";
+import styled from "@emotion/styled";
+import Button from "@mui/material/Button";
+interface Props {
+  bgcolor: string;
+}
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div<Props>`
+  background-color: ${(props) => props.bgcolor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
+function App() {
+  return (
+    <Father>
+      <Box bgcolor="blue">
+        <Text>hello</Text>
+      </Box>
+      <Circle bgcolor="yellow">
+        <Text>Mr.</Text>
+      </Circle>
+      <Button variant="contained" color="error">
+        Kang
+      </Button>
+    </Father>
+  );
+}
+export default App;
